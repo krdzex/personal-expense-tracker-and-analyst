f0d616abfab255a43a96ea8ccd98f74935470f5f
Krsto Kostic <kostic.krsto@gmail.com>
2021-11-01 12:26:58 +0100

    Fixing css mistakes

--- frontend/src/Components/DashBoard/Dashboard.js
+++ frontend/src/Components/DashBoard/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import DashboardBoxes from './DashboardBoxes';
 import DashBoardChart from './DashBoardChart';
 import DashBoardTable from './DashBoardTable';
 
 const Dashboard = () => {
     return (
-        <div>
-            <DashboardBoxes />
+        <div className="dashBoardWrapper">
             <div className="dashBoardBottom">
                 <DashBoardChart />
                 <DashBoardTable />
